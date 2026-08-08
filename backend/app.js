e49110ec4a91eee36e326c90141c6cb7c5f070c7
Aman <abhayrajput112005@gmail.com>
2026-08-08 13:51:21 +0530

    Update CORS origin for submit endpoint
    update

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://aman-wxd-byhp.onrender.com",
+      "https://aman-wxd-byhp.onrender.com/submit",
     ],
    
   })
